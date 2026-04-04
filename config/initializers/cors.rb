3cb12270f7773191b51d0f7030226063986c79a0
caiotarso <caiotarso7@gmail.com>
2026-04-04 19:02:54 -0300

    feat: add rack-cors for handling CORS and configure allowed origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,22 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    allowed_origins = ENV.fetch("FRONTEND_ORIGINS", "")
+      .split(",")
+      .map(&:strip)
+      .reject(&:empty?)
+
+    local_dev_origins = [
+      %r{\Ahttp://localhost:\d+\z},
+      %r{\Ahttp://127\.0\.0\.1:\d+\z}
+    ]
+
+    origins(*allowed_origins, *local_dev_origins)
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+end
